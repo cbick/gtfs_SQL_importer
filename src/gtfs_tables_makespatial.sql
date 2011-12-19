939aeb36700be1258f09d96c1fb87d67e843e6e8
Aaron Ogle <atogle@gmail.com>
2011-12-19 12:40:48 -0500

    add spatial indexes

--- src/gtfs_tables_makespatial.sql
+++ src/gtfs_tables_makespatial.sql
@@ -6,6 +6,9 @@ SELECT AddGeometryColumn('gtfs_stops', 'the_geom', 4326, 'POINT', 2);
 -- Update the the_geom column
 UPDATE gtfs_stops SET the_geom = ST_SetSRID(ST_MakePoint(stop_lon, stop_lat), 4326);
 
+-- Create spatial index
+CREATE INDEX "gtfs_stops_the_geom_gist" ON "gtfs_stops" using gist ("the_geom" gist_geometry_ops);
+
 -- Create new table to store the shape geometries
 CREATE TABLE gtfs_shape_geoms (
   shape_id    text
@@ -23,5 +26,8 @@ SELECT shape.shape_id, ST_SetSRID(ST_MakeLine(shape.the_geom), 4326) As new_geom
     ORDER BY shape_id, shape_pt_sequence
   ) AS shape
 GROUP BY shape.shape_id;
+
+-- Create spatial index
+CREATE INDEX "gtfs_shape_geoms_the_geom_gist" ON "gtfs_shape_geoms" using gist ("the_geom" gist_geometry_ops);
 
 COMMIT;
